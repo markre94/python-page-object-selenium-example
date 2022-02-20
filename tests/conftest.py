672076aecdc552909dca004689275b4f4259edfc
Marcin Krężel <54006852+markre94@users.noreply.github.com>
2022-02-20 17:32:48 +0100

    Allure steps modification

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,11 +2,16 @@ import json
 import os
 from dataclasses import dataclass
 
+import allure
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+
+from utils.log import setup_custom_logger
+
+logger = setup_custom_logger()
 
 
 @dataclass
@@ -16,36 +21,50 @@ class TestConfig:
     headless: bool
 
 
-@pytest.fixture(scope='session')
+@allure.step("Load configuration data")
 def config():
     config_file = os.path.join("/Users/marcin94/PycharmProjects/sauce_demo_ui_tests", 'config.json')
     with open(config_file) as f:
         return TestConfig(**json.load(f))
 
 
-@pytest.fixture
-def init_driver(config):
+@allure.step("Browser config")
+def browser_config():
+    logger.info("Brower options configuration")
+    options = Options()
+    options.add_argument('--no-sandbox')  # # Bypass OS security model
+    options.add_argument('disable-infobars')
+    options.add_argument("--disable-extensions")
+    options.add_argument("--start-fullscreen")
+    options.add_argument('--disable-gpu')
+    return options
+
+
+@allure.step("Drivers setup")
+def driver_setup(options):
+    driver_exec_path = ChromeDriverManager().install()
+    s = Service(driver_exec_path)
+    driver = webdriver.Chrome(service=s, options=options)
+    logger.info("ChromeDriver setup.")
+    return driver
+
+
+@pytest.fixture()
+def init_driver():
     """Fixture used for initializing of the driver."""
 
-    if config.browser == "Chrome":
+    con = config()
+    if con.browser == "Chrome":
+        options = browser_config()
 
-        options = Options()
-        options.add_argument('--no-sandbox')  # # Bypass OS security model
-        options.add_argument('disable-infobars')
-        options.add_argument("--disable-extensions")
-        options.add_argument("--start-fullscreen")
-        options.add_argument('--disable-gpu')
-
-        if config.headless:
+        if con.headless:
             options.add_argument("--headless")
 
-        driver_exec_path = ChromeDriverManager().install()
-        s = Service(driver_exec_path)
-        driver = webdriver.Chrome(service=s, options=options)
-
+        driver = driver_setup(options)
     else:
-        raise Exception(f'Browser "{config.browser}" is not supported')
+        raise Exception(f'Browser "{con.browser}" is not supported')
 
     yield driver
 
+    logger.info("Closing driver.")
     driver.close()
